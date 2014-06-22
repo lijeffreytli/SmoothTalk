d225205ba45a1571d6fc1abc78ceb3002517edba
Jeffrey Li <lijeffreytli@gmail.com>
2014-06-21 20:43:36 -0500

    Jeffrey Testing: ignore

--- gen/com/example/smoothtalk/R.java
+++ gen/com/example/smoothtalk/R.java
@@ -1109,7 +1109,7 @@ containing a value of this type.
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
-        public static final int btnSendSMS=0x7f05003d;
+        public static final int btnSendSMS=0x7f05003f;
         public static final int button_contacts=0x7f050043;
         public static final int button_enter_number=0x7f050041;
         public static final int button_settings=0x7f050042;
@@ -1156,10 +1156,10 @@ containing a value of this type.
         public static final int split_action_bar=0x7f05001e;
         public static final int submit_area=0x7f050039;
         public static final int tabMode=0x7f050002;
-        public static final int textView1=0x7f05003f;
+        public static final int textView1=0x7f05003e;
         public static final int title=0x7f05002b;
         public static final int top_action_bar=0x7f050020;
-        public static final int txtPhoneNo=0x7f05003e;
+        public static final int txtPhoneNo=0x7f05003d;
         public static final int up=0x7f050021;
         public static final int useLogo=0x7f050003;
         public static final int withText=0x7f05000c;
