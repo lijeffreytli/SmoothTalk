1272a43653ebaa8477e4ad9a11024c98ca40209f
Jeffrey Li <lijeffreytli@gmail.com>
2014-07-23 13:21:06 -0500

    Deleted action-bar files

--- gen/com/example/smoothtalk/R.java
+++ gen/com/example/smoothtalk/R.java
@@ -1218,16 +1218,14 @@ containing a value of this type.
         public static final int abc_search_dropdown_item_icons_2line=0x7f030015;
         public static final int abc_search_view=0x7f030016;
         public static final int abc_simple_decor=0x7f030017;
-        public static final int activity_actionbar_about=0x7f030018;
-        public static final int activity_actionbar_help=0x7f030019;
-        public static final int activity_get_contacts=0x7f03001a;
-        public static final int activity_get_settings=0x7f03001b;
-        public static final int activity_get_test_luck=0x7f03001c;
-        public static final int activity_main=0x7f03001d;
-        public static final int activity_send_to_number=0x7f03001e;
-        public static final int fragment_main=0x7f03001f;
-        public static final int fragment_send_to_number=0x7f030020;
-        public static final int support_simple_spinner_dropdown_item=0x7f030021;
+        public static final int activity_get_contacts=0x7f030018;
+        public static final int activity_get_settings=0x7f030019;
+        public static final int activity_get_test_luck=0x7f03001a;
+        public static final int activity_main=0x7f03001b;
+        public static final int activity_send_to_number=0x7f03001c;
+        public static final int fragment_main=0x7f03001d;
+        public static final int fragment_send_to_number=0x7f03001e;
+        public static final int support_simple_spinner_dropdown_item=0x7f03001f;
     }
     public static final class menu {
         public static final int get_contacts=0x7f0c0000;
